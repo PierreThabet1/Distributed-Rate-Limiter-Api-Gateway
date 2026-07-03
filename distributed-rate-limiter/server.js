bf4afc4767dfa5e8e3b87271e1c83232bfed52d7
Pierre Thabet <pierre.thabet@outlook.com>
2026-07-03 12:08:56 +0000

    Add observation measurements for tracking the load balancing

--- distributed-rate-limiter/server.js
+++ distributed-rate-limiter/server.js
@@ -3,10 +3,13 @@ const express = require('express');
 const redis = require('redis');
 const fs = require('fs');
 const path = require('path');
+const os = require('os');
 
-const app = express();
 const REDIS_HOST = process.env.REDIS_HOST;
 const REDIS_PORT = Number(process.env.REDIS_PORT);
+
+const app = express();
+
 const client = redis.createClient({
     socket: {
         host: REDIS_HOST,
@@ -14,36 +17,30 @@ const client = redis.createClient({
     }
 });
 
-client.on('error', (err) => {
-    console.error('Redis error:', err);
-});
-
 async function connectRedis() {
     const maxAttempts = 10;
-
     for (let attempt = 1; attempt <= maxAttempts; attempt++) {
         try {
             await client.connect();
-            console.log(`Connected to Redis at ${REDIS_HOST}:${REDIS_PORT}`);
+            console.log(`Connected to Redis after ${REDIS_HOST}:${REDIS_PORT}`);
             return;
         } catch (err) {
-            console.error(`Redis connection attempt ${attempt}/${maxAttempts} failed: `, err);
+            console.error(`Redis connection attempt ${attempt}/${maxAttempts} failed:`, err.message);
             if (attempt === maxAttempts) {
-                console.error('Max Redis connection attempts reached. Exiting...');
+                console.error('could not connect to Redis. Exiting.');
                 process.exit(1);
             }
-            await new Promise(resolve => setTimeout(resolve, 2000)); // Wait 2 seconds before retrying
+            await new Promise(resolve => setTimeout(resolve, 2000));
         }
     }
 }
-
 
 const rateLimitScript = fs.readFileSync(path.join(__dirname, 'rate_limiter.lua'), 'utf8');
 
 const RATE_LIMIT = parseInt(process.env.RATE_LIMIT);
 const TIME_WINDOW = parseInt(process.env.TIME_WINDOW);
 
-// Middleware to check rate limit
+// Middleware for rate limiting
 async function rateLimiter(req, res, next) {
     const ip = req.ip;
     try {
@@ -54,10 +51,10 @@ async function rateLimiter(req, res, next) {
         if (allowed === 1) {
             next();
         } else {
-            res.status(429).send('Too Many Requests. Please try again later.');
+            res.status(429).send('Too Many Requests');
         }
     } catch (err) {
-        console.error('Error checking rate limit:', err);
+        console.error('Error occurred while evaluating rate limit script:', err);
         res.status(500).send('Internal Server Error');
     }
 }
@@ -65,11 +62,12 @@ async function rateLimiter(req, res, next) {
 app.use(rateLimiter);
 
 app.get('/', (req, res) => {
-    res.status(200).send('Welcome to the Rate Limited API!');
+    res.status(200).send(`Welcome to the rate-limited API! from ${os.hostname()}`);
 });
 
-const PORT = process.env.PORT;
+
 async function startServer() {
+    const PORT = Number(process.env.PORT);
     await connectRedis();
     app.listen(PORT, () => {
         console.log(`Server is running on port ${PORT}`);
